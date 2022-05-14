8d9a0a0eec1616da0777a7f68e96b7052a915341
osapiyo <osapiyo1@gmail.com>
2022-05-14 09:10:15 +0900

    add code for post and back Home

--- components/NewPost/FormikPostUploader.js
+++ components/NewPost/FormikPostUploader.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import * as Yup from 'yup'
 import { Formik } from 'formik'
 import { Divider } from 'react-native-elements'
+import { NavigationContainer } from '@react-navigation/native'
+import validUrl from 'valid-url'
 
 const PLACEHOLDER_IMG =
   'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'
@@ -12,13 +14,17 @@ const uploadPostSchema = Yup.object().shape({
   caption: Yup.string().max(2200, 'Caption has reached the character limit'),
 })
 
-const FormikPostUploader = () => {
+const FormikPostUploader = ({ navigation }) => {
   const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMG)
 
   return (
     <Formik
       initialValues={{ caption: '', imageUrl: '' }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values) => {
+        console.log(values)
+        console.log('Your post was submitted successfully ')
+        navigation.goBack()
+      }}
       validationSchema={uploadPostSchema}
       validateOnMount={true}
     >
